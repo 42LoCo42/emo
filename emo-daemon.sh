381c66d04126f9d7ca2a55bb934dfcd1ebcf6ad4
Leon Schumacher <leonsch@protonmail.com>
2021-08-16 18:10:59 +0200

    Add emo-daemon.sh

--- emo-daemon.sh
+++ emo-daemon.sh
@@ -1,2 +1,40 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+declare -a queue
+
+error() {
+	echo "error" "$@"
+}
+
+push_queue() {
+    queue+=("$1")
+}
+
+pop_queue() {
+	[ -z "${queue[*]}" ] && error "empty queue" && return
+    echo "next ${queue[0]}"
+    queue=("${queue[@]:1}")
+}
+
+print_queue() {
+	for i in "${!queue[@]}"; do
+		echo "queued $i ${queue[i]}"
+	done
+}
+
+coproc yell { exec yell "$@"; }
+declare yell_PID
+trap 'kill $yell_PID' EXIT
+exec 1>&"${yell[1]}"- # write stdout to yell
+
+while read -ru "${yell[0]}" cmd args; do
+	case "$cmd" in
+		exit)  break ;;
+		queue) print_queue ;;
+		add)   push_queue "$args" ;;
+		next)  pop_queue ;;
+		clear) queue=() ;;
+		*)     error "unknown command"
+	esac
+done
